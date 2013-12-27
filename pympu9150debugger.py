025533b5def6e15beee3553979e45dfc32c27946
Tae Kim <tae@zikto.com>
2013-12-27 10:36:26 +0900

    add connecting function for all major OS. List ports and open

--- pympu9150debugger.py
+++ pympu9150debugger.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
 import serial
+from serial.tools import list_ports
 import sys
+import os
+
 
 class Mpu9150:
     def __init__(self):
@@ -9,20 +12,70 @@ class Mpu9150:
 
     def command(self, line):
         if line == "help":
-
             print "accel - print accel data"
-
-        elif line == "accel":
-            self.ToggleAccel()
+            print "connect - connect to module"
+            print "sample - change thie sampling rate"
+        elif line == "connect":
+            self.connect()
+        elif line == "sample":
+            self.change_sampling_rate()
         elif line =="":
-            print "say something"
+            print "say something"     
+        elif line == "quit":
+            print "quitting"
+                
         else:
             print line+" : unkonwn command. type help"
 
     def ToggleAccel(self):
         print "toggle"
-                
-            
+
+    def connect(self):
+        ports = list(self.serial_ports())
+        for idx,val in enumerate(ports):
+            print str(idx) + ". "+val
+
+        num = raw_input("Select the port for the MPU-9150 : ")
+
+        self.port = ports[int(num)]
+
+        self.ser = serial.Serial(self.port , 115200 , timeout=1)
+        #self.ser.open()
+
+        if self.ser.isOpen():
+            print "Connected..."
+        else:
+            print "Port busy"
+
+    def change_sampling_rate(self):
+        print "changing sampling rates to : "
+        print "1. 100,000"
+        print "2. 50,000"
+        print "3. 25,000"
+        print "4. 20,000"
+        print "5. 10,000"
+        command = raw_input("Choose sampling rate(1-5) : ")
+
+        self.ser.write("inv"+command)
+        
+
+    def serial_ports(self):
+        """
+        Returns a generator for all available serial ports
+        """
+        if os.name == 'nt':
+            # windows
+            for i in range(256):
+                try:
+                    s = serial.Serial(i)
+                    s.close()
+                    yield 'COM' + str(i + 1)
+                except serial.SerialException:
+                    pass
+        else:
+            # unix
+            for port in list_ports.comports():
+                yield port[0]
 
 if __name__ == "__main__":
     command = ""
